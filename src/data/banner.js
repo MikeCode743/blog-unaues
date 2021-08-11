1d93b3a70fa9f90119fd130a799e2d66bc7cb072
Miguel Hernández <69060948+MikeCode743@users.noreply.github.com>
2021-08-10 23:02:28 -0600

    add link 12/08

--- src/data/banner.js
+++ src/data/banner.js
@@ -3,7 +3,7 @@ export const bannerText = {
   description:
     "Mejorar el conocimiento sobre los componentes básicos del saneamiento y la necesidad de implementar medidas sanitarias.",
   fecha: "12 de Agosto 2021",
-  hora: "8:00 am a 12:00 md ",
+  hora: "8:30 am a 4:00 pm ",
   image: "https://source.unsplash.com/WIYtZU3PxsI/2400x1600", // https://source.unsplash.com/{PHOTO ID}/{WIDTHxHEIGHT}
   imgText: "main image description",
 };
@@ -12,7 +12,7 @@ export const bannerButtons = [
   {
     id: 1, // Enlace de Teams
     text: "Reunirse Ahora",
-    link: "https://teams.microsoft.com/l/meetup-join/19%3aIrKy91XifR7bTJ8iicBDMBal2uNRFCROF3YLHqrOTGA1%40thread.tacv2/1627329950679?context=%7b%22Tid%22%3a%2288a68981-9703-4695-8414-38d411c54732%22%2c%22Oid%22%3a%22635e5e6a-798f-4f6f-b43a-af1c91eb96a3%22%7d",
+    link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTkzN2JjN2YtZjdiNi00MWM1LWEzMDUtMTI2MmI2YmYyODky%40thread.v2/0?context=%7b%22Tid%22%3a%2288a68981-9703-4695-8414-38d411c54732%22%2c%22Oid%22%3a%22635e5e6a-798f-4f6f-b43a-af1c91eb96a3%22%7d",
     color: "primary",
     icon: "groups",
     disable: false,
@@ -20,15 +20,15 @@ export const bannerButtons = [
   {
     id: 2, //Formulario de asistencia
     text: "Asistencia",
-    link: "https://forms.gle/qSUZRDCBUNJUNa3p7",
+    link: "https://forms.gle/eaJQZ62eoKrtJUKSA",
     color: "secondary",
     icon: "how_to_reg",
-    disable: false,
+    disable: true,
   },
   {
     id: 3, //Formulario para entregas de tareas
     text: "Entregar Tarea",
-    link: "#",
+    link: "https://forms.gle/dtiNjbjEW9RJh6aV7",
     color: "default",
     icon: "task",
     disabled: true,
