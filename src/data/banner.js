b8295dd6254d8a47fa4553bb9795f454b393ab7b
mikehub743 <mikehdz743@gmail.com>
2021-11-06 10:32:26 -0600

    update banner text

--- src/data/banner.js
+++ src/data/banner.js
@@ -1,30 +1,30 @@
 export const bannerText = {
-  title: "Taller 7 - Plan Nacional de Educación Ambiental",
-  description:
-    "Taller enfocado en enfatizar en la proteccion de grupos vulnerables",
-  fecha: "Jueves 4 de noviembre 2021",
+  title: "Proximas Giras de Campo",
+  description: "Reserva de Apaneca",
+  fecha: "Jueves 11 de noviembre 2021",
   hora: "8:00 am a 4:00 pm ",
-  image: "https://source.unsplash.com/6jYoil2GhVk/2400x1600", // https://source.unsplash.com/{PHOTO ID}/{WIDTHxHEIGHT}
+  // image: "https://i.ytimg.com/vi/TWuLUBFEGwg/maxresdefault.jpg",
+  image: "https://source.unsplash.com/bMhWy296Ao0/2400x1600", // https://source.unsplash.com/{PHOTO ID}/{WIDTHxHEIGHT}
   imgText: "main image description",
 };
 
 export const bannerButtons = [
-  {
-    id: 1, // Enlace de Teams
-    text: "Reunirse Ahora",
-    link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTkzN2JjN2YtZjdiNi00MWM1LWEzMDUtMTI2MmI2YmYyODky%40thread.v2/0?context=%7b%22Tid%22%3a%2288a68981-9703-4695-8414-38d411c54732%22%2c%22Oid%22%3a%22635e5e6a-798f-4f6f-b43a-af1c91eb96a3%22%7d",
-    color: "primary",
-    icon: "groups",
-    disabled: false,
-  },
-  {
-    id: 2, //Formulario de asistencia
-    text: "Asistencia",
-    link: "https://forms.gle/UpS3GVSobnUbvshX9",
-    color: "secondary",
-    icon: "how_to_reg",
-    disabled: false,
-  },
+  // {
+  //   id: 1, // Enlace de Teams
+  //   text: "Reunirse Ahora",
+  //   link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTkzN2JjN2YtZjdiNi00MWM1LWEzMDUtMTI2MmI2YmYyODky%40thread.v2/0?context=%7b%22Tid%22%3a%2288a68981-9703-4695-8414-38d411c54732%22%2c%22Oid%22%3a%22635e5e6a-798f-4f6f-b43a-af1c91eb96a3%22%7d",
+  //   color: "primary",
+  //   icon: "groups",
+  //   disabled: false,
+  // },
+  // {
+  //   id: 2, //Formulario de asistencia
+  //   text: "Asistencia",
+  //   link: "https://forms.gle/UpS3GVSobnUbvshX9",
+  //   color: "secondary",
+  //   icon: "how_to_reg",
+  //   disabled: false,
+  // },
   // {
   // id: 3, //Formulario para entregas de tareas
   // text: "Entregar Tarea",
